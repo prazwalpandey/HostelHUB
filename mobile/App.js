5347c619ac558b7e2200c49be35ecd758ec9cd2c
pradeepbhattarai08 <pradeepbhattarai08@gmail.com>
2024-02-16 22:05:12 +0545

    SetUP

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Click Learn More To Get Alert</Text>
+      <Button
+        title='Learn More'
+        color='green'
+        onPress={()=>alert('Welcome to Mobile app development using react native.')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
