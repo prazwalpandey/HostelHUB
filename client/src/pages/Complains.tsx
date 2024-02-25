88e6891b51d06097ea53add69adbc0794a060aa0
Reetwiz Amatya <reetwizamatya02@gmail.com>
2024-02-25 13:51:04 +0545

    complains

--- client/src/pages/Complains.tsx
+++ client/src/pages/Complains.tsx
@@ -1,65 +1,18 @@
-import AdminSidebar from '../components/Sidebar';
-import { IconType } from "react-icons";
-import { FaHotel,  FaPeopleGroup} from "react-icons/fa6";
-import { IoNotifications } from "react-icons/io5";
-import { CgNotes } from "react-icons/cg";
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import ComplaintsList from '../components/ComplaintsList'; // Import ComplaintsList component
 
-
-const Complains = () => {
-    const widgetItemsData = [
-        {
-            value: 300, heading: "Rooms", Icon: FaHotel
-        },
-        {
-            value: 400, heading: "Students", Icon: FaPeopleGroup
-        },
-        {
-            value: 200, heading: "Complains", Icon: CgNotes
-        },
-        {
-            value: 300, heading: "Notices", Icon: IoNotifications
-        }
-    ];
-
+const Admindashboard = () => {
     return (
-        <div className='adminContainer'>
-            <AdminSidebar />
-            <main className='dashboard'>
-                <section className="widgetcontainer">
-                    {widgetItemsData.map((item, index) => (
-                        <WidgetItem
-                            key={index}
-                            value={item.value}
-                            heading={item.heading}
-                            Icon={item.Icon}
-                        />
-                    ))}
-                </section>
-            </main>
+        <div className="adminContainer">
+            <Sidebar />
+            <div className="w-full p-4">
+                <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+                <ComplaintsList /> {/* Add ComplaintsList component here */}
+                {/* Add other content as needed */}
+            </div>
         </div>
     );
 };
 
-interface WidgetItemProps {
-    heading: string;
-    value: number;
-    Icon: IconType;
-}
-
-const WidgetItem = ({
-    heading,
-    value,
-    Icon,
-}: WidgetItemProps) => (
-    <article className="widget">
-        <div className="widgetInfo">
-           <div className="widgetContent">
-            <p>{heading}</p>
-                <h4>{`${value}`}</h4>
-           </div>
-            <Icon className='icon'/>
-        </div>
-    </article>
-);
-
-export default Complains;
+export default Admindashboard;
