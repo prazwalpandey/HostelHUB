a3841a6dff6da08cc1352e61aa79887448e6d508
prazwalstark <prazwolstark@gmail.com>
2024-02-26 01:00:47 +0545

    sfasdf

--- client/src/components/FileUpload.tsx
+++ client/src/components/FileUpload.tsx
@@ -47,16 +47,34 @@ function FileUpload() {
     setSelectedFile(e.target.files[0]);
   };
 
-  const handleUpload = () => {
-    // Implement your upload functionality here
-    // You can use selectedFile to upload the file
-    console.log("Upload file:", selectedFile);
+  const handleUpload = async () => {
+    try {
+      if (!selectedFile) {
+        console.log("No file selected");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      await fetch("http://localhost:5000/fileupload", {
+        method: "POST",
+        body: formData,
+      });
+
+      console.log("File uploaded successfully!");
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
 
   return (
     <div className="flex flex-col items-center justify-center">
       {!selectedFile && (
-        <label htmlFor="fileInput" className="cursor-pointer bg-blue-500 text-white font-medium px-4 py-2 rounded-md shadow-md hover:bg-blue-600">
+        <label
+          htmlFor="fileInput"
+          className="cursor-pointer bg-blue-500 text-white font-medium px-4 py-2 rounded-md shadow-md hover:bg-blue-600"
+        >
           Select File
           <input
             type="file"
@@ -70,8 +88,13 @@ function FileUpload() {
 
       {selectedFile && (
         <div className="flex flex-row items-center">
-          <p className="mr-2">Selected File: {selectedFile.name.substring(0, 8)}...</p>
-          <button onClick={handleUpload} className="bg-green-500 text-white font-medium px-4 py-2 rounded-md shadow-md hover:bg-green-600">
+          <p className="mr-2">
+            Selected File: {selectedFile.name.substring(0, 8)}...
+          </p>
+          <button
+            onClick={handleUpload}
+            className="bg-green-500 text-white font-medium px-4 py-2 rounded-md shadow-md hover:bg-green-600"
+          >
             Upload
           </button>
         </div>
@@ -81,4 +104,3 @@ function FileUpload() {
 }
 
 export default FileUpload;
-
