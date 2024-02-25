0e4623f19ff15a795d46a43b7f358455eaf506c5
prazwalstark <prazwolstark@gmail.com>
2024-02-25 18:46:18 +0545

    dfsf

--- client/src/pages/Clientregisterroom.tsx
+++ client/src/pages/Clientregisterroom.tsx
@@ -25,172 +25,154 @@ const Clientregisterroom = () => {
   ];
   return (
     <>
-  <div className="adminContainer" style={{ width: "100vw", height: "100vh" }}>
-    <ClientSidebar />
-    <main className="dashboard" style={{ width: "100%", height: "100%"}}>
       <div
-        className="flex flex-col w-full p-4 rounded-lg shadow-md bg-white items-center justify-center h-full"
-        style={{width: "auto", height: "100%", padding:"30px" }}
+        className="adminContainer"
+        style={{ width: "100vw", height: "100vh" }}
       >
-    <h2 className="text-2xl font-semibold text-gray-700 mb-2 text-center">Register For Room Allocation</h2>
-    <hr className="w-full my-4 border-t border-gray-300" />
-        <p
-          className="reminder text-center mb-4"
-          style={{ color: "#333", fontSize: "0.875rem" }}
-        >
-          Reminder: Only one form should be submitted by any one of the members.
-        </p>
-        <div className="flex justify-center">
-      <form className="w-full text-center" onSubmit={handleSubmit}>
-        <div className="mb-6">
-          <label htmlFor="name1" className="text-sm font-medium mb-2 ">
-            Student 1
-          </label>
-          <br />
-          <select
-            id="name1"
-            className="input-style"
-            style={{
-              padding: "0.1rem 1rem",
-              border: "1px solid #ccc",
-              borderRadius: "0.25rem",
-              outline: "none",
-            }}
-            value={student1RollNo}
-            onChange={(e) => setStudent1RollNo(e.target.value)}
+        <ClientSidebar />
+        <main className="dashboard" style={{ width: "100%", height: "100%" }}>
+          <div
+            className="flex flex-col w-full p-4 rounded-lg shadow-md bg-white items-center h-full"
+            style={{ width: "auto", height: "100%", padding: "30px" }}
           >
-            {rollNoOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="mb-6">
-          <label htmlFor="name2" className="text-sm font-medium mb-2">
-            Student 2
-          </label>
-          <br />
-          <select
-            id="name2"
-            className="input-style"
-            style={{
-              padding: "0.1rem 1rem",
-              border: "1px solid #ccc",
-              borderRadius: "0.25rem",
-              outline: "none",
-            }}
-            value={student2RollNo}
-            onChange={(e) => setStudent2RollNo(e.target.value)}
-          >
-            {rollNoOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="mb-6">
-          <label htmlFor="name3" className="text-sm font-medium mb-2">
-            Student 3
-          </label>
-          <br />
-          <select
-            id="name3"
-            className="input-style"
-            style={{
-              padding: "0.1rem 1rem",
-              border: "1px solid #ccc",
-              borderRadius: "0.25rem",
-              outline: "none",
-            }}
-            value={student3RollNo}
-            onChange={(e) => setStudent3RollNo(e.target.value)}
-          >
-            {rollNoOptions.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-medium py-1 px-1 rounded"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-
-
-
-
-
-        {/* <label htmlFor="name1" className="text-sm font-medium mb-2">
-          Student 1
-        </label>
-        <input
-          id="name1"
-          type="text"
-          placeholder="Roll NO. eg, PUL077BCT055"
-          className="input-style"
-          style={{
-            width: "30%",
-            marginBottom: "10px",
-            padding: "0.1rem 1rem",
-            border: "1px solid #ccc",
-            borderRadius: "0.25rem",
-            outline: "none",
-          }}
-        />
-        <label htmlFor="name2" className="text-sm font-medium mb-2">
-          Student 2
-        </label>
-        <input
-          id="name2"
-          type="text"
-          placeholder="Roll NO. eg, PUL077BCT056"
-          className="input-style"
-          style={{
-            width: "30%",
-            marginBottom: "10px",
-            padding: "0.1rem 1rem",
-            border: "1px solid #ccc",
-            borderRadius: "0.25rem",
-            outline: "none",
-          }}
-        />
-        <label htmlFor="name3" className="text-sm font-medium mb-2">
-          Student 3
-        </label>
-        <input
-          id="name3"
-          type="text"
-          placeholder="Roll NO. eg, PUL077BCT057"
-          className="input-style"
-          style={{
-            width: "30%",
-            marginBottom: "10px",
-            padding: "0.1rem 1rem",
-            border: "1px solid #ccc",
-            borderRadius: "0.25rem",
-            outline: "none",
-          }}
-        />
-        <button
-            type="button"
-            className="bg-sky-500 text-white font-medium text-sm px-4 py-2 rounded-md shadow-md hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-          style={{width:"30%"}}>
-            Submit
-          </button> */}
+            <h2 className="text-2xl font-semibold text-gray-700 mb-2 text-center">
+              Register For Room Allocation
+            </h2>
+            <hr className="w-full my-4 border-t border-gray-300" />
+            <p
+              className="reminder text-center mb-4"
+              style={{ color: "#333", fontSize: "0.875rem" }}
+            >
+              Reminder: Only one form should be submitted by any one of the
+              members.
+            </p>
+              <form
+                className="w-full text-center"
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyItems: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+                onSubmit={handleSubmit}
+              >
+                <div
+                  className="mb-6"
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                  }}
+                >
+                  <label htmlFor="name1" className="text-sm font-light mb-2 ">
+                    Student 1
+                  </label>
+                  <select
+                    id="name1"
+                    className="input-style"
+                    style={{
+                      width: "50%",
+                      justifyContent: "center",
+                      padding: "0.1rem 1rem",
+                      border: "1px solid #ccc",
+                      borderRadius: "0.25rem",
+                      outline: "none",
+                    }}
+                    value={student1RollNo}
+                    onChange={(e) => setStudent1RollNo(e.target.value)}
+                  >
+                    {rollNoOptions.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                      <br />
+                <div className="mb-6"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+                >
+                  <label htmlFor="name2" className="text-sm font-light mb-2">
+                    Student 2
+                  </label>
+    
+                  <select
+                    id="name2"
+                    className="input-style"
+                    style={{
+                      width: "50%",
+                      justifyContent: "center",
+                      padding: "0.1rem 1rem",
+                      border: "1px solid #ccc",
+                      borderRadius: "0.25rem",
+                      outline: "none",
+                    }}
+                    value={student2RollNo}
+                    onChange={(e) => setStudent2RollNo(e.target.value)}
+                  >
+                    {rollNoOptions.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+<br />
+                <div className="mb-6"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+                >
+                  <label htmlFor="name3" className="text-sm font-light mb-2">
+                    Student 3
+                  </label>
+          
+                  <select
+                    id="name3"
+                    className="input-style"
+                    style={{
+                      width: "50%",
+                      justifyContent: "center",
+                      padding: "0.1rem 1rem",
+                      border: "1px solid #ccc",
+                      borderRadius: "0.25rem",
+                      outline: "none",
+                    }}
+                    value={student3RollNo}
+                    onChange={(e) => setStudent3RollNo(e.target.value)}
+                  >
+                    {rollNoOptions.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+<br />
+                <button
+                  type="submit"
+                  className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-medium py-1 px-1 rounded"
+                >
+                  Submit
+                </button>
+              </form>
+          </div>
+        </main>
       </div>
-    </main>
-  </div>
-</>
+    </>
   );
 };
 
